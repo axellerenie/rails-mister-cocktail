cc585b6f970e886270c0d6865a59ca7e32b1d145
Axelle Renie <axelle.renie@gmail.com>
2017-03-10 12:21:39 +0100

    Add figaro - Protect my secret data in config/application.yml

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,25 +1,23 @@
 class CocktailsController < ApplicationController
-  before_action :get_cocktail, only: [:show]
+  # before_action :get_cocktail, only: [:show, :edit]
 
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
   def new
     @cocktail = Cocktail.new
-    # @ingredients = @cocktail.ingredients
-    # @description = @cocktail.description
-
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktails_path(@cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
@@ -27,9 +25,9 @@ class CocktailsController < ApplicationController
 
   private
 
-  def get_cocktail
-    @cocktail = Cocktail.find(params[:id])
-  end
+  # def get_cocktail
+  #   @cocktail = Cocktail.find(params[:id])
+  # end
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+  # get "cocktails/new", to: "cocktails#new"
+  # post "cocktails", to: "cocktails#create"
+
+  # get "cocktails", to: "cocktails#index"
+  # get "cocktails/:id", to: "cocktails#show"
+
+  # get "cocktails/:id/doses/new", to: "cocktails#edit"
+  # post "cocktails/:id/doses", to: "cocktails#update"
+
+  resources :cocktails, only: [:show, :index, :new, :create] do
+    resources :doses, only: [:create]
+  end
   root "cocktails#index"
-
-  get "cocktails/new", to: "cocktails#new"
-  post "cocktails", to: "cocktails#create"
-
-  get "cocktails", to: "cocktails#index"
-  get "cocktails/:id", to: "cocktails#show"
 
 end
